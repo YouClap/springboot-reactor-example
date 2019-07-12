91ed999fa14cdfcc74b2562df4b1056925f48a71
Renato Almeida <renato.m.alm@gmail.com>
2019-07-12 13:54:32 +0100

    Fix libraries and User

--- src/main/kotlin/tech/youclap/example/springboot/model/User.kt
+++ src/main/kotlin/tech/youclap/example/springboot/model/User.kt
@@ -13,12 +13,8 @@ data class User(
     @Column("displayName")
     val displayName: String,
 
-    val username: String,
+    val username: String
 
-    @Column("photoURL")
-    val photoURL: String,
-
-    val email: String
 )
 
 // TODO maybe we could use `inline class UserID(val id: Int)` 🤔
